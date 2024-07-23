3ad25d59ae8e783892a94210d076e44d9f3a323c
Albert Batlle <albert.batlle@gmail.com>
2024-07-23 20:20:10 +0200

    Add files via upload

--- bicing_programacion.sql
+++ bicing_programacion.sql
@@ -169,13 +169,30 @@ select * from usuaris;
 -- Frecuencia: Cada hora.
 -- Acción: Enviar notificaciones a los usuarios.
 
+SET FOREIGN_KEY_CHECKS = 0;
+
 create table notificacions(
 	id int primary key auto_increment,
-    usuari_id int,
+    -- usuari_id int,
+    destinatari varchar(100),
+    assumpte varchar(50),
+    missatge text,
+    data_creacio timestamp default current_timestamp,
     bicicleta_id int,
-    tipus_notificacio varchar(50)
+    -- constraint FK_UsuariNotificacions foreign key (usuari_id) references usuari(id),
+    constraint FK_BicicletaNotificacions foreign key (bicicleta_id) references bicicletes(id)
 );
 
+alter table notificacions add column temps_lloguer decimal(7,2);
+alter table notificacions add column mail_usuari varchar(45);
+
+alter table lloguer add constraint FK_lloguerbicicletes foreign key (bicicleta_id) references bicicletes(id);
+alter table lloguer add constraint FK_lloguerusuari foreign key (usuari_id) references usuari(id);
+
+SET FOREIGN_KEY_CHECKS = 1;
+SET GLOBAL event_scheduler=ON;
+
+truncate notificacions;
 
 drop event if exists notificacions;
 delimiter //
@@ -183,17 +200,121 @@ create event notificacions
 on schedule every 1 hour
 do 
 begin
-	insert into comptes values(default, "CompteNova", 2024);
-    select * from comptes;
+	
+declare done boolean default false;    
+declare num_lloguer_id int;
+declare num_usuari_id int;
+declare num_bicicleta_id int;
+declare num_estacio int;
+    
+declare cursor_usuari_bicicleta cursor for
+	SELECT id, usuari_id, bicicleta_id, estacio_final_id
+	from lloguer
+	where estacio_final_id is null;
+	
+declare continue handler for not found set done = true;
+    
+open cursor_usuari_bicicleta;
+    
+bucle_lectura: loop
+	fetch cursor_usuari_bicicleta into num_lloguer_id, num_usuari_id, num_bicicleta_id, num_estacio;
+	if done then
+		leave bucle_lectura;
+	end if;
+        
+	set @temps_lloguer = (timestampdiff(minute, (select inici_lloguer from lloguer where id = num_lloguer_id), current_timestamp()));
+	set @nom_usuari = (select concat_ws(" ", nom, cognom1, cognom2) from usuaris where id = num_usuari_id);
+	set @mail_usuari = (select mail from usuaris where id = num_usuari_id);
+    set @user_id = num_usuari_id;
+    -- set @nul = (SELECT estacio_final_id from lloguer where estacio_final_id is null);
+	-- insert into notificacions (id, destinatari, temps_lloguer, mail_usuari) values (default,@nom_usuari,@temps_lloguer,@mail_usuari);
+    
+    -- if num_estacio is null then
+		
+		 -- 
+         insert into notificacions(destinatari, assumpte, bicicleta_id, temps_lloguer, mail_usuari) values (@nom_usuari, "Venciment Entrega Bicicleta", num_bicicleta_id, @temps_lloguer, @mail_usuari);
+        
+		  -- insert into notificacions(usuari_id, destinatari, assumpte, bicicleta_id, temps_lloguer, mail_usuari) values (num_usuari_id, @nom_usuari, "Venciment Entrega Bicicleta", num_bicicleta_id, @temps_lloguer, @mail_usuari);
+        
+        if @temps_lloguer between 20 and 30 then
+			update notificacions set missatge = (concat("Falten ", (30 - @temps_lloguer), " minuts perquè puguis entregar la teva bicicleta de manera gratuïta.")) where id = (select max(id) from notificacions);
+		elseif @temps_lloguer < 120 then
+			update notificacions set missatge = (concat("Falten ", (120 - @temps_lloguer), " minuts perquè puguis entregar la teva bicicleta sense que et cobrem el sobrecost de 5€.")) where id = (select max(id) from notificacions);
+		else
+			update notificacions set missatge = (concat("El temps de lloguer de la bicicleta és actualment de ", (@temps_lloguer), " minuts. Recora que per cada hora se't cobrarà 5€.")) where id = (select max(id) from notificacions);
+		end if; 
+	-- end if;
+        -- select * from notificacions where usuari_id = num_usuari_id;       
+end loop bucle_lectura;
+
+close cursor_usuari_bicicleta;
+    
+end //
+delimiter ;
+insert into notificacions (id) values (default);
+select * from notificacions;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+insert into lloguer (estacio_origen_id, bicicleta_id, usuari_id) values (179, 3, 5);
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+-- 2- Evento para archivar alquileres antiguos. (1,5 punto)
+-- Crea un evento que se ejecute una vez al año y mueva los registros de alquileres que tengan más de dos años a una tabla de archivo (alquileres_archivo).
+-- Frecuencia: Anual.
+-- Acción: Archivar registros antiguos.
+
+drop table if exists lloguers_arxivats;
+create table lloguers_arxivats like lloguer;
+alter table lloguers_arxivats add column DataSupressio timestamp default current_timestamp;
+alter table lloguers_arxivats add column lloguer_id int;
+
+select * from lloguer;
+select * from lloguers_arxivats;
+
+
+drop event if exists event_arxivar_lloguer;
+delimiter //
+create event event_arxivar_lloguer
+on schedule every 10 second
+do 
+begin
+	declare done boolean default false;
+    
+    declare cursor_lloguerID int;
+    declare cursor_inici_lloguer timestamp;
+    declare cursor_fi_lloguer timestamp;
+    declare cursor_origenID int;
+    declare cursor_finalID int;
+    declare cursor_tarifa decimal(4,2);
+    declare cursor_bicicletaID int;
+    declare cursor_usuari_id int;
+    
+    declare cursor_arxivar_lloguer cursor for
+		select * from lloguer where inici_lloguer < date_sub(CURDATE(), INTERVAL 2 YEAR);
+        
+    declare continue handler for not found set done = true;
+    
+    open cursor_arxivar_lloguer;
+    
+    bucle_lectura: loop
+		fetch cursor_arxivar_lloguer into cursor_lloguerID, cursor_inici_lloguer, cursor_fi_lloguer, cursor_origenID, cursor_finalID, cursor_tarifa, 
+        cursor_bicicletaID, cursor_usuari_id;
+        if done then
+			leave bucle_lectura;
+		end if;
+        
+        insert into lloguers_arxivats values(default, cursor_inici_lloguer, cursor_fi_lloguer, cursor_origenID, cursor_finalID, cursor_tarifa, cursor_bicicletaID, cursor_usuari_id, default, cursor_lloguerID);
+        DELETE FROM lloguer where id = cursor_lloguerID;
+        
+	end loop;
+close cursor_arxivar_lloguer;
+
 end //
 delimiter ;
 
+insert into lloguer (inici_lloguer, estacio_origen_id) values ("1999-04-23", 176);
 
-
-select * from facturacio_has_usuaris;
-
-
-select * from lloguer;
-select * from bicicletes_has_lloguer;
-select * from bicicletes_has_lloguer BL  right join lloguer L on L.id = BL.lloguer_id;
 
